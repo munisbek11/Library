5476c90d3ff30d25bacec6a244345e72f10090a9
munisbek11 <munisbekshomurodov677@gmail.com>
2024-11-26 08:28:15 +0500

    added new change

--- index.js
+++ index.js
@@ -14,10 +14,18 @@ const app = express();
 app.use(express.json());
 app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
+const {logger, loggerJSON} = require("./service/logger")
 
 const PORT = process.env.PORT || 4000;
 
 connectDB()
+
+logger.error("Logger error")
+logger.info("logger info")
+logger.debug("logger debug")
+logger.warn("logger warn")
+loggerJSON.info("loggerjson info")
+loggerJSON.error("loggerJson error")
 
 
 app.use(bookRouter);
